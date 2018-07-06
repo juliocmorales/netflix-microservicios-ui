22d8ad2e2653f9f4dda064eced28c37c1bb94708
juliocmorales <40004698+juliocmorales@users.noreply.github.com>
2018-07-06 13:30:15 -0500

    primer commit

---
+++ src/main/java/com/neoris/microservicios/NetflixMicroserviciosUiApplication.java
@@ -0,0 +1,14 @@
+package com.neoris.microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class NetflixMicroserviciosUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixMicroserviciosUiApplication.class, args);
+	}
+}
